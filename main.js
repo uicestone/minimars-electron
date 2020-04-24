11ee8115caeae24ac56d4058c25f1e85c6316295
Uice Lu <uicestone@gmail.com>
2020-04-24 10:01:40 +0800

    printBand.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
-const Usb = require("escpos-usb");
 
 function createWindow() {
   // Create the browser window.
@@ -26,24 +25,7 @@ function createWindow() {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.allowRendererProcessReuse = false;
-app
-  .whenReady()
-  .then(createWindow)
-  .then(async function () {
-    const device = new Usb();
-    device.open(function (error) {
-      device.write("\x1Bia\x03"); // switch mode
-      device.write("^II");
-      device.write("^ONdata1\x00");
-      device.write("^DI\x0B\x00136****6334");
-      device.write("^ONdata2\x00");
-      device.write("^DI\x10\x002020-04-24 00:00");
-      device.write("^ONqr\x00");
-      device.write("^DI\x0B\x00136****6334");
-      device.write("^ID");
-      device.write("^FF");
-    });
-  });
+app.whenReady().then(createWindow);
 
 // Quit when all windows are closed.
 app.on("window-all-closed", function () {
